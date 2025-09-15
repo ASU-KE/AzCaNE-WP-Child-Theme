49a1fe5da0319ce6a80c2e9a48812b9718b407eb
John Cox <john.k.cox@asu.edu>
2025-09-15 14:49:15 -0700

    doc ready h1 script init

--- src/js/child-theme.js
+++ src/js/child-theme.js
@@ -1,1 +1,5 @@
 /* Child theme JavaScript */
+document.addEventListener('DOMContentLoaded', function() {
+    const subtitle = document.querySelector('.navbar .title')
+    subtitle.prepend('<h1 id="site-title">AzCaNE</h1>')
+})
