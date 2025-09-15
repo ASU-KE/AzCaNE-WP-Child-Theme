2a2776eec57157ffc0c7cd15d4c94f461f326ccd
John Cox <john.k.cox@asu.edu>
2025-09-15 15:04:16 -0700

    event listener

--- src/js/child-theme.js
+++ src/js/child-theme.js
@@ -1,23 +1,25 @@
 /* Child theme JavaScript */
-// Function to wait for an element to be available in the DOM
+document.addEventListener('DOMContentLoaded', function() {
+    waitForElement('.navbar .title').then((element) => {
+        element.prepend('<h1 id="site-title">AzCaNE</h1>')
+    })
+})
+
+
+
 function waitForElement(selector) {
     return new Promise((resolve) => {
         const observer = new MutationObserver((mutations, observer) => {
-            const element = document.querySelector(selector);
+            const element = document.querySelector(selector)
             if (element) {
-                observer.disconnect();
-                resolve(element);
+                observer.disconnect()
+                resolve(element)
             }
-        });
+        })
 
         observer.observe(document.body, {
             childList: true,
             subtree: true,
-        });
-    });
+        })
+    })
 }
-
-// Usage
-waitForElement('.navbar .title').then((element) => {
-    element.prepend('<h1 id="site-title">AzCaNE</h1>');
-});
