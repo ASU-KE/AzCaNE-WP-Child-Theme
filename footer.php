e0d60c6735e16ad0352daf20e398889a4f894050
John Cox <john.k.cox@asu.edu>
2025-09-24 10:36:30 -0700

    linkedin icon update

--- footer.php
+++ footer.php
@@ -26,8 +26,7 @@ defined( 'ABSPATH' ) || exit;
 					<div class="social-media-wrapper">
 						<div class="menu-example-social-media-container">
 							<nav aria-label="Social Media" class="nav">
-								<a aria-label="linkedin" id="menu-item-23" class="nav-link" href="https://www.linkedin.com/company/azcane/"><svg class="svg-inline--fa fa-square-linkedin" data-prefix="fab" data-icon="square-linkedin" role="img" viewBox="0 0 448 512" aria-hidden="true" data-fa-i2svg=""><path fill="currentColor" d="M100.28 448H7.4V149.26h92.88zm-46.44-339.6c-29.89 0-54.16-24.27-54.16-54.16S24.95 0 54.84 0c29.89 0 54.16 24.27 54.16 54.16s-24.27 54.16-54.16 54.16zm391.76 339.6h-92.88V302.4c0-34.96-12.48-58.83-43.66-58.83-23.83 0-37.92 16.06-44.14 31.56-2.27 5.51-2.84 13.16-2.84 20.83v152.58h-92.88s1.2-247.4 0-273.6h92.88v38.76c12.29-18.88 34.36-45.92 83.66-45.92 61.02 0 106.84 39.84 106.84 125.12z"></path></svg></a> 
-							</nav>
+								<a aria-label="linkedin" id="menu-item-695" class="nav-link" href="https://www.linkedin.com/company/azcane/"><svg class="svg-inline--fa fa-linkedin" data-prefix="fab" data-icon="linkedin" role="img" viewBox="0 0 448 512" aria-hidden="true" data-fa-i2svg=""><path fill="currentColor" d="M416 32L31.9 32C14.3 32 0 46.5 0 64.3L0 447.7C0 465.5 14.3 480 31.9 480L416 480c17.6 0 32-14.5 32-32.3l0-383.4C448 46.5 433.6 32 416 32zM135.4 416l-66.4 0 0-213.8 66.5 0 0 213.8-.1 0zM102.2 96a38.5 38.5 0 1 1 0 77 38.5 38.5 0 1 1 0-77zM384.3 416l-66.4 0 0-104c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9l0 105.8-66.4 0 0-213.8 63.7 0 0 29.2 .9 0c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9l0 117.2z"></path></svg><!-- <span class="fab fa-linkedin"></span> --></a>
 						</div>					
 					</div>
 				</div>
