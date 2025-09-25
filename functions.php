67c5bef560827968526a90e9c16e7c0b2f85a78b
John Cox <john.k.cox@asu.edu>
2025-09-25 12:34:30 -0700

    rm child js

--- functions.php
+++ functions.php
@@ -21,8 +21,8 @@ function uds_wordpress_child_scripts() {
 	$css_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/dist/css/child-theme.min.css' );
 	wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/dist/css/child-theme.min.css', array( 'uds-wordpress-styles' ), $css_child_version );
 
-	$js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/dist/js/theme.min.js' );
-	wp_enqueue_script( 'uds-wordpress-child-scripts', get_stylesheet_directory_uri() . '/dist/js/theme.min.js', array( 'jquery' ), $js_child_version );
+	//$js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/dist/js/theme.min.js' );
+	//wp_enqueue_script( 'uds-wordpress-child-scripts', get_stylesheet_directory_uri() . '/dist/js/theme.min.js', array( 'jquery' ), $js_child_version );
 }
 add_action( 'wp_enqueue_scripts', 'uds_wordpress_child_scripts' );
 
